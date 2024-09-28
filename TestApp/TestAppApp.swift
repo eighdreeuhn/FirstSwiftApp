279c813293564f2554fdaf0a88cf46ac415cbeb6
Adrian Gioia <afgioia@gmail.com>
2024-09-28 16:59:18 -0500

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Adrian F Gioia on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
